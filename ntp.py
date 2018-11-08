3460c9d129b5322cc234785b82e98426fa290436
gwvsol <gwvsol@gmail.com>
2018-11-08 16:14:50 +0200

    Update library ntp.py

--- ntp.py
+++ ntp.py
@@ -16,16 +16,13 @@ try:
 except:
     import errno
 
-# (date(2000, 1, 1) - date(1900, 1, 1)).days * 24*60*60
 NTP_DELTA = 3155673600
-# NTP server
 host = "pool.ntp.org"
 
 def getntp():
     print('Get UTC time from NTP server...')
     NTP_QUERY = bytearray(48)
     NTP_QUERY[0] = 0x1b
-   # Handling an unavailable NTP server error
     try:
         addr = socket.getaddrinfo(host, 123)[0][-1]
     except OSError: # as exc:
@@ -35,7 +32,7 @@ def getntp():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.settimeout(1)
     res = s.sendto(NTP_QUERY, addr)
-   # Handling NTP server long response error
+
     try:
         msg = s.recv(48)
     except OSError as exc:
@@ -47,8 +44,6 @@ def getntp():
     val = struct.unpack("!I", msg[40:44])[0]
     return val - NTP_DELTA
 
-# There's currently no timezone support in MicroPython, so
-# utime.localtime() will return UTC time (as if it was .gmtime())
 def settime():
    tm = time.localtime(getntp())
    tm = tm[0:3] + (0,) + tm[3:6] + (0,)
