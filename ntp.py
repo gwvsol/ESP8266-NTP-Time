82a4370c38f2a029575c81baab8dc581f15b938c
gwvsol <gwvsol@gmail.com>
2019-03-07 23:27:00 +0200

    changing the library file

--- ntp.py
+++ ntp.py
@@ -1,20 +1,8 @@
-import machine
-try:
-   import utime as time
-except:
-   import time
-try:
-    import usocket as socket
-except:
-    import socket
-try:
-    import ustruct as struct
-except:
-    import struct
-try:
-    import uerrno as errno
-except:
-    import errno
+from time import localtime
+from socket import socket, getaddrinfo, AF_INET, SOCK_DGRAM
+from struct import unpack
+from errno import ETIMEDOUT
+from machine import RTC
 
 NTP_DELTA = 3155673600
 host = "pool.ntp.org"
@@ -24,28 +12,26 @@ def getntp():
     NTP_QUERY = bytearray(48)
     NTP_QUERY[0] = 0x1b
     try:
-        addr = socket.getaddrinfo(host, 123)[0][-1]
+        addr = getaddrinfo(host, 123)[0][-1]
     except OSError: # as exc:
         #if exc.args[0] == -2:
             print('Connect NTP Server: Error resolving pool NTP')
             return 0
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s = socket(AF_INET, SOCK_DGRAM)
     s.settimeout(1)
     res = s.sendto(NTP_QUERY, addr)
 
     try:
         msg = s.recv(48)
     except OSError as exc:
-        if exc.args[0] == errno.ETIMEDOUT:
+        if exc.args[0] == ETIMEDOUT:
             print('Connect NTP Server: Request Timeout')
             s.close()
             return 0
     s.close()
-    val = struct.unpack("!I", msg[40:44])[0]
+    val = unpack("!I", msg[40:44])[0]
     return val - NTP_DELTA
 
 def settime():
-   tm = time.localtime(getntp())
-   tm = tm[0:3] + (0,) + tm[3:6] + (0,)
-   machine.RTC().datetime(tm)
+   RTC().datetime(localtime(getntp()))
    print(time.localtime())
